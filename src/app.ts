f9a789908414777bec5a35baa321340beda6853a
vidal77 <vidal77-lc@hotmail.com>
2023-06-05 20:01:33 -0300

    termino la conexion

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+
+//rutas
+//app.use();
+
+
+export default app
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("")
+import app from './app'
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
